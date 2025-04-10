969d9a276466ba21e55c37ce5cd3d4d41281675b
goldentoaste <rayg2375@gmail.com>
2025-04-10 16:49:17 -0700

    ??

--- src/lib/firebase/CanvasFirebaseController.ts
+++ src/lib/firebase/CanvasFirebaseController.ts
@@ -1,7 +1,7 @@
 
 import { Timestamp } from "@firebase/firestore/lite";
 import { deleteLines, fetchLines, uploadLines, type FirebaseLine } from "./api";
-import { Line } from "$lib/Components/canvas/CanvasController.svelte";
+import { Line } from "$lib/Components/canvas/CanvasLogics.svelte.ts";
 
 
 
