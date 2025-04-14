282042fa5efd298d2a5c82cc0c120e0f5db40735
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-04-14 12:44:28 -0700

    teaser site barebones

---
+++ teaser/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-mona-sans)', 'ui-sans-serif', 'system-ui'],
+				mono: ['var(--font-geist-mono)', 'ui-monospace'],
+			},
+		},
+	},
+};
