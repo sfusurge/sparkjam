c8d85d6a2f2fdb9419f322f60ba7997d7f02babf
Christina <christinaraganit@gmail.com>
2025-04-14 12:13:43 -0700

    added sparkjam logo

---
+++ src/lib/components/landing/Landing.svelte
@@ -0,0 +1,11 @@
+<main
+  class="max-w-screen max-h-screen w-screen h-screen flex flex-col justify-center items-center"
+>
+  <img
+    src="home/sparkjam-logo.svg"
+    alt="SparkJaam logo"
+    class="max-w-[720px] w-full"
+  />
+
+  <span>Applications will open at this date</span>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,36 @@
 <script lang="ts">
-    import HomePageStuff from "$lib/components/physics_stuff/home_pagge_stuff.svelte";
+  import Landing from "$lib/components/landing/Landing.svelte";
+  import HomePageStuff from "$lib/components/physics_stuff/home_pagge_stuff.svelte";
 
-
-	import { transitionType } from "$lib/components/utils/ViewTransitionState.svelte.ts";
-
+  import { transitionType } from "$lib/components/utils/ViewTransitionState.svelte.ts";
 </script>
 
+<Landing></Landing>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
 
 <a
-	href="canvas"
-	onclick={() => {
-		transitionType.transition = 'fadeLeft';
-	}}>Canvas</a
+  href="canvas"
+  onclick={() => {
+    transitionType.transition = "fadeLeft";
+  }}>Canvas</a
 >
 
 <a
-	href="game"
-	onclick={() => {
-		transitionType.transition = 'up';
-	}}>Game</a
+  href="game"
+  onclick={() => {
+    transitionType.transition = "up";
+  }}>Game</a
 >
 
 <div class="tester">BALH BLAH</div>
 
-
-
-
-<HomePageStuff/>
+<HomePageStuff />
 <h1>rcfgvhgbvhb</h1>
-
-<style>
-	.tester {
-		position: absolute;
-		right: 0;
-		border: blue 6px solid;
-	}
-	a {
-		border: 2px solid coral;
-		padding: 1rem;
-		box-sizing: border-box;
-	}
-</style>
-
 
 <!-- 
 TOpics
@@ -53,3 +40,16 @@ TOpics
 * update application
 
 -->
+
+<style>
+  .tester {
+    position: absolute;
+    right: 0;
+    border: blue 6px solid;
+  }
+  a {
+    border: 2px solid coral;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+</style>
