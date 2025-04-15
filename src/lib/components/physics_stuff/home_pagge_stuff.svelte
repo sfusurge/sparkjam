fb1bf1727339d104066a8dbd943b0de138de6778
goldentoaste <rayg2375@gmail.com>
2025-04-14 20:27:56 -0700

    refactor physics code

--- src/lib/components/physics_stuff/home_pagge_stuff.svelte
+++ src/lib/components/physics_stuff/home_pagge_stuff.svelte
@@ -1,378 +1,206 @@
 <script lang="ts">
-	import {
-		Engine,
-		Render,
-		Runner,
-		Common,
-		Composite,
-		Vertices,
-		Svg,
-		Bodies,
-		Vector,
-		Mouse,
-		MouseConstraint,
-		Body,
-	} from "matter-js";
-	import { onMount } from "svelte";
-	import "./pathseg.js";
-	import PolyDecomp from "poly-decomp";
+    import {
+        Engine,
+        Render,
+        Runner,
+        Common,
+        Composite,
+        Svg,
+        Bodies,
+        Vector,
+        Mouse,
+        MouseConstraint,
+        Body,
+        World,
+    } from "matter-js";
+    import { onMount } from "svelte";
+    import "./pathseg.js";
+    // @ts-ignore
+    import PolyDecomp from "poly-decomp";
 
-	let width = $state(500);
-	let height = $state(500);
-	let container = $state<HTMLDivElement>();
-	let svgPaths: string[] = [
-		"./home/arrow.svg",
-		"./home/asterisk.svg",
-		"./home/box.svg",
-		"./home/bundle.svg",
-		"./home/otter.svg",
-		"./home/.svg",
-		"./home/arrow.svg",
-		"./home/pencil.svg",
-	];
+    interface Props {
+        width: number;
+        height: number;
+    }
 
-	onMount(async () => {
-		Common.setDecomp(PolyDecomp);
-		// return;
-		const engine = Engine.create();
-		const world = engine.world;
+    const { width, height }: Props = $props();
 
-		const render = Render.create({
-			element: container,
-			engine,
-			options: {
-				width,
-				height,
-				background: "white",
-				wireframes: false,
-			},
-		});
+    let container = $state<HTMLDivElement>();
 
-		Render.run(render);
+    const svgs: Record<
+        "arrow" | "asterisk" | "box" | "otter" | "pencil" | "star" | "surge" | "bundle",
+        { shape: string; img: string; verticies?: Vector[][] | undefined }
+    > = {
+        arrow: {
+            img: "./home/arrow.svg",
+            shape: "./home/arrowShape.svg",
+        },
+        asterisk: {
+            img: "./home/asterisk.svg",
+            shape: "./home/asterisk.svg",
+        },
+        box: {
+            img: "./home/box.svg",
+            shape: "./home/boxShape.svg",
+        },
+        otter: {
+            img: "./home/otter.svg",
+            shape: "./home/otterShape.svg",
+        },
+        pencil: {
+            img: "./home/pencil.svg",
+            shape: "./home/pencilShape.svg",
+        },
+        star: {
+            img: "./home/star.svg",
+            shape: "./home/starShape.svg",
+        },
+        surge: {
+            img: "./home/surge.svg",
+            shape: "./home/surgeShape.svg",
+        },
+        bundle: {
+            img: "./home/bundle.svg",
+            shape: "./home/bundleShape.svg",
+        },
+    };
 
-		const runner = Runner.create();
+    let world: World | undefined = undefined;
 
-		Runner.run(runner, engine);
+    function addShape(x: number, y: number, verts: Vector[][], texturePath: string) {
+        if (!world) return;
 
-		// fetch svgs in js
-		async function loadSvgs(url: string) {
-			const text = await (await fetch(url)).text();
-			return new DOMParser().parseFromString(text, "image/svg+xml");
-		}
+        const b = Bodies.fromVertices(
+            x,
+            y,
+            verts,
+            {
+                render: {
+                    sprite: {
+                        texture: texturePath,
+                        xScale: 0.8,
+                        yScale: 0.8,
+                    },
+                },
+                density: 1.2,
+                restitution: 0,
+                frictionAir: 1,
+            },
+            true,
+        );
+        Body.set(b, "position", {
+            x,
+            y,
+        });
+        Composite.add(world, b);
+    }
 
-		function Svg2Vertices(d: Document) {
-			const paths = d.querySelectorAll("path");
+    onMount(async () => {
+        Common.setDecomp(PolyDecomp);
+        // return;
+        const engine = Engine.create();
+        world = engine.world;
 
-			const res: Vector[][] = [];
-			for (const p of paths) {
-				res.push(Svg.pathToVertices(p, 30));
-			}
-			return res;
-		}
+        const render = Render.create({
+            element: container,
+            engine,
+            options: {
+                width,
+                height,
+                background: "white",
+                wireframes: false,
+            },
+        });
 
-		let arrow: Vector[][] | undefined = undefined;
-		let asterisk: Vector[][] | undefined = undefined;
-		let box: Vector[][] | undefined = undefined;
-		let bundle: Vector[][] | undefined = undefined;
-		let otter: Vector[][] | undefined = undefined;
-		let pencil: Vector[][] | undefined = undefined;
-		let star: Vector[][] | undefined = undefined;
-		let surge: Vector[][] | undefined = undefined;
+        Render.run(render);
 
-		const loadingPromises: Promise<void>[] = [
-			loadSvgs("./home/arrowShape.svg").then((d) => {
-				arrow = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/asterisk.svg").then((d) => {
-				asterisk = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/boxShape.svg").then((d) => {
-				box = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/bundleShape.svg").then((d) => {
-				bundle = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/otterShape.svg").then((d) => {
-				otter = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/pencilShape.svg").then((d) => {
-				pencil = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/starShape.svg").then((d) => {
-				star = Svg2Vertices(d);
-			}),
-			loadSvgs("./home/surgeShape.svg").then((d) => {
-				surge = Svg2Vertices(d);
-			}),
-		];
+        const runner = Runner.create();
 
-		await Promise.all(loadingPromises); //
+        Runner.run(runner, engine);
 
-		Composite.add(
-			world,
-			Bodies.fromVertices(
-				Math.random() * width,
-				-300,
-				asterisk!,
-				{
-					render: {
-						strokeStyle: "#B3E8FF",
-						lineWidth: 1,
-						fillStyle: "#B3E8FF",
-					},
-					label: "Asterisk",
-					density:0.6
+        // fetch svgs in js
+        async function loadSvgs(url: string) {
+            const text = await (await fetch(url)).text();
+            return new DOMParser().parseFromString(text, "image/svg+xml");
+        }
 
-				},
-				true,
-			),
-		);
+        function Svg2Vertices(d: Document) {
+            const paths = d.querySelectorAll("path");
 
-		Composite.add(
-			world,
-			Bodies.fromVertices(
-				220,
-				-450,
-				otter!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/otter.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "Otter",
-					density:0.6
-				},
-				true,
-			),
-		);
-		const boxBody = Bodies.fromVertices(
-			-0,
-			0,
-			box!,
-			{
-				render: {
-					sprite: {
-						texture: "./home/box.svg",
-						xScale: 0.8,
-						yScale: 0.8,
-					},
-				},
-				label: "Box",
-				density:1
+            const res: Vector[][] = [];
+            for (const p of paths) {
+                res.push(Svg.pathToVertices(p, 30));
+            }
+            return res;
+        }
 
-			},
-			true,
-		);
-		Body.set(boxBody, "position", { x: Math.random() * width, y: Math.random() *-200 });
-		Composite.add(world, boxBody);
+        // load vertex shapes of each shape
+        await Promise.all(
+            Object.values(svgs).map((item) =>
+                loadSvgs(item.shape).then((d) => {
+                    item.verticies = Svg2Vertices(d);
+                }),
+            ),
+        );
 
-		Composite.add(
-			world,
-			Bodies.fromVertices(
-				Math.random() * width,
-				-300,
-				surge!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/surge.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "Surge",
-					density:1
-
-				},
-				true,
-			),
-		);
-				const b = Bodies.fromVertices(
-				Math.random( ) * 500,
-				-300,
-				bundle!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/bundle.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "bundle",
-					density:1
-
-				},
-				true,
-			);
-
-		Body.set(b, "position", { x: Math.random() * width, y: Math.random() *-80 });
-
-		Composite.add(
-			world,
-			b,
-		);
-
-		const p1 = Bodies.fromVertices(
-			Math.random() * width,
-			-300,
-			pencil!,
-			{
-				render: {
-					sprite: {
-						texture: "./home/pencil.svg",
-						xScale: 0.8,
-						yScale: 0.8,
-					},
-				},
-				label: "pencil2",
-				density:1
-
-			},
-			true,
-		);
-
-		Body.set(p1, "position", { x: Math.random() * width, y:  Math.random() * -200 });
-		Composite.add(world, p1);
-		const p2 = Bodies.fromVertices(
-			Math.random() * width,
-			-300,
-			pencil!,
-			{
-				render: {
-					sprite: {
-						texture: "./home/pencil.svg",
-						xScale: 0.8,
-						yScale: 0.8,
-					},
-				},
-				label: "pencil2",
-				density:1
-
-			},
-			true,
-		);
-
-		Body.set(p2, "position", { x: Math.random() * width, y:  Math.random() * -200 });
-		Composite.add(world, p2);
-
-		Composite.add(
-			world,
-			Bodies.fromVertices(
-				Math.random() * width,
-				Math.random() *-300,
-				arrow!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/arrow.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "arrow2",
-					density:1
-
-				},
-				true,
-			),
-		);
-
-		Composite.add(
-			world,
-			Bodies.fromVertices(
-				Math.random() * width,
-				Math.random() *-300,
-				arrow!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/arrow.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "arrow1",
-					density:1
-
-				},
-				true,
-			),
-		);
+        // pre-defined pile
+        const rng = Math.random;
+        addShape(rng() * width, -300, svgs.asterisk.verticies!, svgs.asterisk.shape);
+        addShape(220, -450, svgs.otter.verticies!, svgs.otter.shape);
+        addShape(rng() * width, rng() * -200, svgs.box.verticies!, svgs.box.shape);
+        addShape(rng() * width, -300, svgs.surge.verticies!, svgs.surge.shape);
+        addShape(rng() * width, -80, svgs.bundle.verticies!, svgs.bundle.shape);
+        addShape(rng() * width, -80, svgs.bundle.verticies!, svgs.bundle.shape);
+        addShape( Math.random() * width, -300, svgs.pencil.verticies!, svgs.pencil.shape);
+        addShape( Math.random() * width, -300, svgs.pencil.verticies!, svgs.pencil.shape);
+        addShape( Math.random() * width, -200, svgs.arrow.verticies!, svgs.arrow.shape);
+        addShape( Math.random() * width, -200, svgs.arrow.verticies!, svgs.arrow.shape);
+        addShape( Math.random() * width, -300, svgs.star.verticies!, svgs.star.shape);
 
 
-		const s = Bodies.fromVertices(
-				Math.random() * width,
-				-300,
-				star!,
-				{
-					render: {
-						sprite: {
-							texture: "./home/star.svg",
-							xScale: 0.8,
-							yScale: 0.8,
-						},
-					},
-					label: "star",
-					density:0.6
+        // world boundry
+        Composite.add(world, [
+            Bodies.rectangle(width, 10000, 10, 20000, {
+                isStatic: true,
+                render: { visible: false },
+            }),
 
-				},
-				true,
-			);
+            Bodies.rectangle(0, 10000, 10, 20000, {
+                isStatic: true,
+                render: { visible: false },
+            }),
+            Bodies.rectangle(width / 2, height - 10, width, 10, {
+                isStatic: true,
+                render: { visible: false },
+            }),
+        ]);
 
-		Body.set(s, "position", { x: Math.random() * width, y: -100 });
+        // Mouse control
+        const mouse = Mouse.create(render.canvas);
+        const mouseCons = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: true,
+                    strokeStyle:"grey"
+                },
+            },
+        });
 
-		Composite.add(
-			world,
-			s,
-		);
+        Composite.add(world, mouseCons);
 
-		// world boundry
-		Composite.add(world, [
-			Bodies.rectangle(width, 10000, 10, 20000, {
-				isStatic: true,
-				render: { visible: false },
-			}),
+        render.mouse = mouse;
 
-			Bodies.rectangle(0, 10000, 10, 20000, { isStatic: true, render: { visible: false } }),
-			Bodies.rectangle(width / 2, height - 10, width, 10, {
-				isStatic: true,
-				render: { visible: false },
-			}),
-		]);
-
-		// Mouse control
-		const mouse = Mouse.create(render.canvas);
-		const mouseCons = MouseConstraint.create(engine, {
-			mouse: mouse,
-			constraint: {
-				stiffness: 0.2,
-				render: {
-					visible: true,
-				},
-			},
-		});
-
-		Composite.add(world, mouseCons);
-
-		render.mouse = mouse;
-
-		Render.lookAt(render, {
-			min: { x: 0, y: 0 },
-			max: { x: width, y: height },
-		});
-	});
+        Render.lookAt(render, {
+            min: { x: 0, y: 0 },
+            max: { x: width, y: height },
+        });
+    });
 </script>
 
 <div
-	class="physicsContainer"
-	bind:this={container}
-	style="width:{width}px; height:{height}px;"
+    class="physicsContainer"
+    bind:this={container}
+    style="width:{width}px; height:{height}px;"
 ></div>
-
-<style>
-
-</style>
