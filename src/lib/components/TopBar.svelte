26d76394b0cd6249f43e1c4a5b5ced2b12bb262d
goldentoaste <rayg2375@gmail.com>
2025-04-18 04:42:39 -0700

    top wip

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { pushState } from "$app/navigation";
     import RainbowButton from "$lib/components/RainbowButton.svelte";
 
     interface NavBarProps {
@@ -11,25 +12,57 @@
     const { items }: NavBarProps = $props();
 
     let width = $state(0);
+    let extended = $state(false);
 </script>
 
 <svelte:window bind:innerWidth={width} />
 
 <nav class="navContainer">
-    {#each items as item}
-        <button class="navBtn">
-            <a href={item.target}
-                ><span class="navLabel">
-                    {item.label}
-                </span></a
-            >
-        </button>
-    {/each}
+    {#if width <= 768}{/if}
 
-    <RainbowButton style="margin-left:auto" disableBottomBorder>TICKETS</RainbowButton>
+    <div class="navRow">
+        {#if width > 768}
+            {#each items as item}
+                <button class="navBtn">
+                    <a href={item.target}
+                        ><span class="navLabel">
+                            {item.label}
+                        </span></a
+                    >
+                </button>
+            {/each}
+        {:else}
+            <button
+                onclick={
+                    ()=>{
+                        pushState('', {
+                        showItem:true
+                    })
+                    }
+                }
+                ><img
+                    src={extended ? "cross.svg" : "/bars.svg"}
+                    alt="more options"
+                    style="width: 2rem; margin-left:1.25rem;"
+                /></button
+            >
+        {/if}
+        <RainbowButton style="margin-left:auto" disableBottomBorder>TICKETS</RainbowButton>
+    </div>
 </nav>
 
 <style>
+    .content {
+    }
+    .contentContainer {
+    }
+
+    .navRow {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
+
     nav.navContainer {
         position: fixed;
         top: 0;
@@ -41,7 +74,7 @@
         max-width: var(--widthLimit);
 
         display: flex;
-        flex-direction: row;
+        flex-direction: col;
 
         border-bottom: 2px solid var(--black);
         background-color: var(--white);
