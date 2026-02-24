6e07a2cca68239f358f66c2f621d82a1b40d13b8
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2026-02-24 15:40:19 -0800

    cursor plus text fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,26 @@
 </script>
 
 <div class="bg-[#F2CB25] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] h-screen w-screen flex justify-center items-center m-0 overflow-hidden relative">
-    <div class="relative">
-        <img src="/sj_logo.svg" alt="SparkJam Logo" />
-        <img src="/star.svg" alt="Star" class="absolute top-9 -right-5 animate-spin [animation-duration:1.5s]" />
+    <div class="relative w-auto">
+        <img src="/sj_logo.svg" alt="SparkJam Logo" class="w-full h-auto" />
+        <img src="/star.svg" alt="Star" class="absolute top-[12%] -right-[2%] w-[12%] animate-spin [animation-duration:1.5s]" />
     </div>
 
-    <div class="absolute bottom-10 left-10 text-[#000] font-['PP_Mori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px]">
-        <h6>More info</h6>
-        <h6>April 2nd</h6>
-    </div>
-
-    <div class="absolute bottom-10 right-10 text-[#000] font-['PP_Mori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px] flex flex-row gap-[20px]">
-        <div class="flex flex-col">
-            <h6>Learning Week</h6>
-            <h6>May 11 - 15</h6>
-        </div>
-        <div>
-            <h6>Designing Week</h6>
-            <h6>May 16 - 23</h6>
+    <div class="absolute inset-0 flex flex-col md:flex-row justify-end md:justify-between items-start md:items-end p-10 pointer-events-none gap-4 md:gap-0 w-auto">
+        <div class="text-[#000] font-['PP_Mori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px] pointer-events-auto">
+            <h6>More info</h6>
+            <h6>April 2nd</h6>
         </div>
 
+        <div class="text-[#000] font-['PP_Mori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px] flex flex-row gap-[20px] pointer-events-auto">
+            <div class="flex flex-col">
+                <h6>Learning Week</h6>
+                <h6>May 11 - 15</h6>
+            </div>
+            <div>
+                <h6>Designing Week</h6>
+                <h6>May 16 - 23</h6>
+            </div>
+        </div>
     </div>
 </div>
